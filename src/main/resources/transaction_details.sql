cc796c2a93672fb07a64e1960ea07aae0fa8c019
NITISH-9851KUMAR <nitish983577@gmail.com>
2025-06-21 17:14:46 +0530

    Nitiya Bank

--- src/main/resources/transaction_details.sql
+++ src/main/resources/transaction_details.sql
@@ -1,4 +1,6 @@
 CREATE TABLE transaction
+# transaction table Name: Nitish31004, follow this format
+# AC/H First Name + List Five Digit of PRN
 (
     date_time   VARCHAR(20),
     details     VARCHAR(20),
